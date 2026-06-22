13de602e35e70812a2523d683c5c508f6212ab61
이고운 <cloud94623@gamil.com>
2026-06-22 22:40:00 +0900

    파이어베이스 키 실제 내용으로 강제 덮어쓰기

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Firebase 콘솔에서 프로젝트 설정 후 아래 정보를 채워주세요.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBE4s5yaTrBJwLNvIelRpZxqdI7z-sY95w",
+  authDomain: "bobmookza.firebaseapp.com",
+  projectId: "bobmookza",
+  storageBucket: "bobmookza.firebasestorage.app",
+  messagingSenderId: "325504639664",
+  appId: "1:325504639664:web:1602b99fcc596ac8675175",
+  measurementId: "G-X5CCMSZT5L"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app;
